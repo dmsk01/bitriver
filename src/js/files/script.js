649dacd18a9e9e79c078536e21d76c8032907730
dmsk01 <petrov3.14@mail.ru>
2023-04-04 22:36:36 +0300

    add header active link

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -5,3 +5,45 @@
 //   customDays: ["пн", "вт", "ср", "чт", "пт", "сб", "вс"],
 //   customMonths: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
 // });
+
+window.onload = function () {
+  // Lines animation
+  if (document.getElementById("lines")) {
+    let svgMorpheus = new SVGMorpheus("#lines");
+    let timeoutInstance;
+
+    const idArray = ["line-1", "line-2", "line-3"];
+    let index = 0;
+
+    function onIconChange() {
+      if (index == idArray.length - 1) {
+        index = 0;
+      } else {
+        index++;
+      }
+      clearTimeout(timeoutInstance);
+      svgMorpheus.to(
+        idArray[index],
+        { duration: 10000, easing: "linear", rotation: "none" },
+        launchTimer
+      );
+    }
+
+    function launchTimer() {
+      timeoutInstance = setTimeout(onIconChange, 1000);
+    }
+
+    launchTimer();
+  }
+
+  // Active header link
+  const links = document.querySelectorAll(".menu__link");
+  const page = window.location.pathname.substring(1);
+  if (links.length > 0) {
+    links.forEach((link) => {
+      link.getAttribute("href") === page
+        ? link.classList.add("active")
+        : link.classList.remove("active");
+    });
+  }
+};
